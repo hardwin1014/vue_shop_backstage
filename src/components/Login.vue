d23733ff5f5b62ac1e5eed208ae50441d98727c8
codewxh <914067438@qq.com>
2021-05-27 21:34:38 +0800

    搭建成功

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login_container">
+  </div>
+</template>
+
+<script>
+export default {
+  // 定义属性
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.login_container {
+  background-color: #2b5b6b;
+  height: 100%;
+}
+</style>
